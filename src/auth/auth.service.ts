2b151427774a0f10e9faa6b65d6a125764c4874d
Suwat <107973351+flukeSuwat@users.noreply.github.com>
2024-08-06 18:24:28 +0700

    d

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,8 +12,6 @@ export class AuthService {
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.userService.findOneUsername(username);
-    console.log(password);
-
     if (user && (await bcrypt.compare(password, user.password))) {
       const { password, ...result } = user;
       return result;
